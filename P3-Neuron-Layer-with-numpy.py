8369bed9674aca1d821767ac90e91691fd657e00
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-12-04 22:18:22 +0200

    update P3-Neuron-Layer-with-numpy

--- P3-Neuron-Layer-with-numpy.py
+++ P3-Neuron-Layer-with-numpy.py
@@ -4,21 +4,32 @@ Creates a simple layer of 3 neurons, with 4 inputs, with numpy dor product.
 
 import numpy as np 
 
-#shap: 1*4
-inputs = [1.0, 2.0, 3.0, 2.5]
 
-#shap: 4*3
-weights = [[0.2,  0.5,  -0.26],
-           [0.8, -0.91, -0.27],
-           [-0.5, 0.26,  0.17],
-           [1.0, -0.5,   0.87]]
+inputs = [1.0, 2.0, 3.0, 2.5]
+#shap: 4*1 (col vector)
+inputs = np.array(inputs).T
+inputs = inputs.reshape(4,1)
+
+#shap: 3*4
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+weights = np.array(weights)
+
 
 biases = [2.0, 3.0, 0.5]
+#shape: 3*1 (col vector)
+biases = np.array(biases).T
+biases = biases.reshape(3,1)
 
 
-output = np.dot(inputs, weights) + biases
+# 3x4 * 4x1 + 3x1
+# 3x1 + 3x1
+# 3x1
+# col vector
+output = np.dot(weights, inputs) + biases
 
 
-print(output)
-#shap: 1*3
-#[4.8   1.21  2.385]
+print(output, output.shape)
+#shap: 3x1
+#[4.8   1.21  2.385].T
